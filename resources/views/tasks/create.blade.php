f42c4865ddef0ab186eefc434967beb4138eb32c
shintaro.tahara <t.shintaro0623@gmail.jp>
2020-06-14 11:34:25 +0000

    git dekinai

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,6 +10,10 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
