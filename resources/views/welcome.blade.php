c373af1898942229f0ed3a04602e40f68a85dce5
shintaro.tahara <t.shintaro0623@gmail.jp>
2020-06-21 06:25:50 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,20 @@
                     @endauth
                 </div>
             @endif
+            
+            @extends('layouts.app')
 
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Microposts</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endsection
+            
+            
+            
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
